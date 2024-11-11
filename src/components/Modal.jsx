28461515efacf1cb664797facc027dd3233b2c40
Vedant Khare <kharevedant05@gmail.com>
2024-11-11 12:42:54 +0530

    Edits

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = ({ onClose, toggle }) => {
   if (!modalRoot) return null;
 
   return createPortal(
-    <div className="text-white-800 fixed inset-0 bg-background/60 backdrop-blur-sm flex items-center justify-center">
+    <div className="z--10 text-white-800 fixed inset-0 bg-background/60 backdrop-blur-sm flex items-center justify-center">
       <div className="bg-background/20 border border-accent/30 border-solid backdrop-blur-[6px] py-8 px-6 xs:px-10 sm:px-16 rounded shadow-glass-inset text-center space-y-8">
         <p className="font-light">Do you like to play the background music?</p>
         <div className="flex items-center justify-center space-x-4">
